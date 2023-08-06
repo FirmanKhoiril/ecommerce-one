8a21c59feaefe3caf2b6778ab5f0a4686f17b885
firmankhoiril <firmankhoiril13@gmail.com>
2023-08-06 19:52:23 +0700

    test

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -8,7 +8,7 @@ const Basket = () => {
     <section
       className={`${
         activeBasket ? "translate-y-[0%]" : "translate-y-[-120%]"
-      } transition duration-300 w-full fixed top-16 right-1 shadow-[0px_2px_4px_2px] shadow-black/10 max-w-xs bg-white px-4 py-2 rounded-b-lg overflow-y-auto max-h-96 h-full z-10 md:right-2  lg:right-10`}
+      } transition duration-300 w-full fixed top-16 right-1 shadow-[0px_2px_4px_2px] shadow-black/10 max-w-[260px] md:max-w-xs bg-white px-4 py-2 rounded-b-lg overflow-y-auto max-h-80 h-full z-10 md:right-2  lg:right-6`}
     >
       test
     </section>
--- components/Card.tsx
+++ components/Card.tsx
@@ -19,22 +19,23 @@ interface ICardProduct {
 
 const Card = ({ name, price, normalPrice, city, details, image, discount, tags, slug: { current } }: ICardProduct) => {
   return (
-    <Link href={`/${current}`} className="w-[200px] bg-white group shadow-md hover:shadow-black/10 rounded-t-lg relative h-80 flex items-start flex-col gap-1">
-      <Image src={urlFor(image[0]?.asset?._ref).url()} loading="lazy" quality={100} alt={image[0]?.alt} width={190} height={120} className="object-cover" />
-      <div className="absolute top-0 left-0 h-10 w-10 text-sm text-center bg-yellow-400 shadow-md rounded-br-md ">
+    <Link href={`/${current}`} className="w-[190px] bg-white group shadow-md hover:shadow-black/10 rounded-t-lg relative h-72 flex items-start flex-col gap-1">
+      <Image src={urlFor(image[0]?.asset?._ref).width(300).url()} loading="lazy" quality={100} alt={image[0]?.alt} width={190} height={150} className="object-contain h-[150px]" />
+      <div className="absolute top-0 left-0 h-10 w-10 text-sm text-center rounded-tl-md bg-yellow-400 shadow-md rounded-br-md ">
         <p className="text-red-600">{discount}%</p>
         <p className="text-white">OFF</p>
       </div>
       <div className="px-2 py-1 flex flex-col gap-[2px]">
-        <h1 className=" text-md">{name}</h1>
+        <h1 className=" text-base">{name}</h1>
         <div className="flex items-center gap-3 text-sm">
           <p className="text-slate-500 relative">
             Rp{normalPrice}
             <span className="absolute w-[110%] h-[1px] -right-1 bottom-[10px] bg-black/40"></span>
           </p>
+
           <p className="text-primary tracking-wide">Rp{price}</p>
         </div>
-        <p className="text-slate-600 tracking-widest text-[15px] uppercase">KOTA {city}</p>
+        <p className="text-slate-600 tracking-widest text-[14px] uppercase">KOTA {city}</p>
       </div>
     </Link>
   );
